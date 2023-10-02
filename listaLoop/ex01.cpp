32c82bd2810b1e1d54c71184ea899520fcf9aaf7
whoami micael <62766013+MicaelRPereiraGTI@users.noreply.github.com>
2023-10-02 20:58:53 -0300

    lista loop

---
+++ listaLoop/ex01.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    for (int x = 1; x <= 100; x++)
+    printf(" %d", x);
+
+    return 0;
+}
---
+++ listaLoop/ex04.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 50; i >= 5; i--){
+    	if(i % 2 != 0){
+    		printf(" %i", i);
+		};
+	};
+
+    return 0;
+}
